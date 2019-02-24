b049ee9546fe0e8737b1fcc93e1a91912ee1eb6f
AlexanderSemenov92 <42918575+AlexanderSemenov92@users.noreply.github.com>
2019-02-24 21:03:54 +0200

    Убрал позиционирование на лого

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,10 @@
 $('.grow-form label').click(function () {
-    $('.grow-form label .radio').css('opacity', '1');
+    // $('.grow-form label .radio').removeClass('active');
+    // $(this).addClass('active');
     $('.grow-form label').removeClass('focus');
     $(this).addClass('focus');
+});
+$('.grow-form label .radio').click(function () {
+    $('.grow-form label .radio').removeClass('active');
+    $(this).addClass('active');
 });
