65f8331903cf571d1bcdb06e3fb305c2090b963a
AlexanderSemenov92 <42918575+AlexanderSemenov92@users.noreply.github.com>
2019-02-24 17:24:49 +0200

    --- Auto-Git Commit ---

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,7 @@
 $('.grow-form label').click(function () {
     $('.grow-form label .radio').css('opacity', '1');
+    $(this).addClass('focus');
+    $('.grow-form label').removeClass('focus');
 });
+
+//, .grow-form label img
